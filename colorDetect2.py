4d09c809ab8fef7ebac1f85c52ce6f4fe6871abb
floraWLM <lemengwangflora@163.com>
2024-03-19 21:44:26 -0400

    Dataset imported and evaluated

--- colorDetect2.py
+++ colorDetect2.py
@@ -1,5 +1,6 @@
 import cv2
 import math 
+import array
 
 # zero is for the first webcam， 1 is the second
 # for macbook use 1 as the front webcam
@@ -7,21 +8,20 @@ cap = cv2.VideoCapture(1)
 # set the pixel size of the cam
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+color_names = []
+color_rgbs = []
 
-# variables storing the bgr value for colors
-red = (0, 0, 255)
-orange = (0,128,255)
-yellow = (0,255,255)
-green = (0,255,0)
-blue = (255,0,0)
-purple = (255,0,127)
-pink = (204,153,255)
-white = (255,255,255)
-black = (0,0,0)
-gray = (128,128,128)
-brown = (0,76,153)
-colorAll = (red,orange,yellow,green,blue,purple,pink,white,black,gray,brown)
-colorName = ("RED","ORANGE","YELLOW","GREEN","BLUE","PURPLE","PINK","WHITE","BLACK","GRAY","BROWN")
+with open("data.txt") as file:
+    for line in file.readlines():
+        line = line.strip().split("\t")
+        color_names.append(line[0])
+        rgb = line[2].removeprefix("(").removesuffix(")").split(",")
+        rgb = [eval(num) for num in rgb]
+        color_rgbs.append(rgb)
+
+
+print(color_names)
+print(color_rgbs)
 
 # the while loop enable us to take multiple images
 # which enable us to form a video
@@ -46,20 +46,20 @@ while cap.isOpened:
     color = "Undefined"
     i = 0
     # search for the closest color stored
-    for c in colorAll:
-        d = math.sqrt(pow(c[0]-b,2)+pow(c[1]-g,2)+pow(c[2]-r,2))
+    for c in color_rgbs:
+        d = math.sqrt(pow(c[0]-r,2)+pow(c[1]-g,2)+pow(c[2]-b,2))
         if d < dMin:
             dMin = d
-            color = colorName[i]
+            color = color_names[i]
             storedValue = c
         elif d == dMin:
             sizedMin = math.sqrt(pow(storedValue[0],2)+pow(storedValue[1],2)+pow(storedValue[2],2))
-            sized = math.sqrt(pow(b,2)+pow(g,2)+pow(r,2))
-            if sized < sizedMin:
-                color = colorName[i]
+            sized = math.sqrt(pow(r,2)+pow(g,2)+pow(b,2))
+            if sized <= sizedMin:
+                color = color_names[i]
                 storedValue = c
         i = i + 1
-        
+
     print(color)
     print(pixel_center)
     cv2.putText(frame, color, (10,50), 0, 1, (b,g,r), 2)
